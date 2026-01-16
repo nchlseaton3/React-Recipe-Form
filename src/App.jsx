b5e90b8f7514d4e7088ca325057dadab41235f5a
Nicholas Eaton <nchlseaton3@gmail.com>
2026-01-16 11:20:44 -0500

    Scaffold recipe submission form component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import RecipeSubmissionForm from "./components/RecipeSubmissionform"
+
+function App() {
+  return (
+    <>
+      <RecipeSubmissionForm />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/RecipeSubmissionform.jsx
@@ -0,0 +1,11 @@
+import "./RecipeSubmissionForm.css"
+
+const RecipeSubmissionForm = () => {
+  return (
+    <div className="container">
+      <h1>Recipe Submission Form</h1>
+    </div>
+  )
+}
+
+export default RecipeSubmissionForm
